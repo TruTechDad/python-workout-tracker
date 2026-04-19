ee43d257f8d0c60965ecbc2906e1152c3d08e1dd
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2026-04-19 17:39:32 -0400

    First version working

--- workout_tracker.py
+++ workout_tracker.py
@@ -1,14 +1,8 @@
 print("Welcome to your Workout Tracker 💪")
 
-while True:
-    exercise = input("\nEnter exercise (or press q to quit): ")
-    
-    if exercise == 'q':
-        print("Exiting Workout Tracker. Goodbye!")
-        break
-    
-    weight = input("Enter weight: ")
-    reps = input("Enter reps: ")
+exercise = input("Enter exercise: ")
+weight = input("Enter weight: ")
+reps = input("Enter reps: ")
 
 print("\nWorkout Saved!")
 print("Exercise:", exercise)
