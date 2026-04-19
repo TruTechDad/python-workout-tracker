0858e5f3900cd0ec53452cc38e1e5816c9ded2f4
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2026-04-19 17:45:59 -0400

    Added loop to allow multiple workouts

--- workout_tracker.py
+++ workout_tracker.py
@@ -1,8 +1,12 @@
 print("Welcome to your Workout Tracker 💪")
 
-exercise = input("Enter exercise: ")
-weight = input("Enter weight: ")
-reps = input("Enter reps: ")
+while True:
+    exercise = input("\nEnter exercise (or press q to quit): ")
+    if exercise == 'q':
+        print("Exiting Workout Tracker. Goodbye!")
+        break
+    weight = input("Enter weight: ")
+    reps = input("Enter reps: ")
 
 print("\nWorkout Saved!")
 print("Exercise:", exercise)
