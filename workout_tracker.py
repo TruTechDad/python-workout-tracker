28d21e85f112504a66b33daecc0536b54468f479
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2026-04-19 15:46:52 -0400

    Initial CLI workout tracker - user input working

---
+++ workout_tracker.py
@@ -0,0 +1,10 @@
+print("Welcome to your Workout Tracker 💪")
+
+exercise = input("Enter exercise: ")
+weight = input("Enter weight: ")
+reps = input("Enter reps: ")
+
+print("\nWorkout Saved!")
+print("Exercise:", exercise)
+print("Weight:", weight)
+print("Reps:", reps)
