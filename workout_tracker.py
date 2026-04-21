e4b3f5bf3b240db09400635b6ebd07775e1d8622
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2026-04-21 19:37:14 -0400

    Saved workouts to JSON file

--- workout_tracker.py
+++ workout_tracker.py
@@ -1,14 +1,35 @@
+import json
+
 print("Welcome to your Workout Tracker 💪")
 
+# Initialize an empty list to store workouts
+try:
+    with open('workouts.json', 'r') as file:
+        workouts = json.load(file)
+except FileNotFoundError:
+    workouts = []
+
 while True:
-    exercise = input("\nEnter exercise (or press q to quit): ")
+    exercise = input("\nEnter exercise (or type 'q' to quit): ")
+
     if exercise == 'q':
-        print("Exiting Workout Tracker. Goodbye!")
+        # Save workouts to a JSON file before exiting
+        with open('workouts.json', 'w') as file:
+            json.dump(workouts, file, indent=4)
+
+        print("\nWorkouts saved to file. Exiting...")
+        print("Goodbye 👋")
         break
+
     weight = input("Enter weight: ")
     reps = input("Enter reps: ")
 
-print("\nWorkout Saved!")
-print("Exercise:", exercise)
-print("Weight:", weight)
-print("Reps:", reps)
+    workout = {
+        "exercise": exercise,
+        "weight": weight,
+        "reps": reps
+    }
+
+    workouts.append(workout)
+
+    print("\nWorkout Saved!")
