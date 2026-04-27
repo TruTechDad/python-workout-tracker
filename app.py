2bf73f334dffff34ee9be06422078b3299f667f3
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2026-04-27 19:13:05 -0400

    Created Flask API for workouts

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+#Load workouts from file
+def load_workouts():
+    try:
+        with open("workouts.json", "r") as file:
+            return json.load(file)
+    except: 
+         return
+    
+#Save workouts to file
+def save_workouts(workouts):
+    with open("workouts.json", "w") as file:
+        json.dump(workouts, file)
+
+#Get all workouts
+@app.route("/workouts", methods=["GET"])
+def get_workouts():
+    workouts = load_workouts()
+    return jsonify(workouts)
+
+#Post a new workout
+@app.route("/workouts", methods=["POST"])
+
+def add_workout():
+
+    workouts = load_workouts()
+
+    data = request.json
+
+    workout = {
+        "exercise": data["exercise"],
+        "weight": data["weight"],
+        "reps": data["reps"]
+    }
+
+    workouts.append(workout)
+    save_workouts(workouts)
+
+    return jsonify({"message": "Workout added!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
