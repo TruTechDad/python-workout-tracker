e981bf9c0d7669a6b94b0c97a9fb66d450d87f78
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2026-05-10 14:04:59 -0400

    Prepare to deploy

--- app.py
+++ app.py
@@ -84,7 +84,10 @@ def generate_workout():
     return jsonify(workout_plan)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
